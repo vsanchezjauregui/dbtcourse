547104d45ab6b9f13c3b85c3f78d2030b621881a
Victor Jauregui <vsanchezjauregui@gmail.com>
2024-05-26 11:15:31 +0100

    added variables macros

---
+++ macros/logging.sql
@@ -0,0 +1,3 @@
+{% macro learn_logging() %}
+  {# {{ log('call your dad', info=True) }} #}
+{% endmacro %}
---
+++ macros/variables.sql
@@ -0,0 +1,5 @@
+{% macro learn_variables() %}
+  {% set your_name_jinja = 'victor' %}
+  {{ log('Hello ' ~ your_name_jinja, info=True) }}
+  {{ log('Hello dbt user ' ~ var('user_name','NO USERNAME IS SET!!') ~ '!', info=True) }}
+{% endmacro %}
