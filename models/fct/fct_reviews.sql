48ede58c4521a05ac3a6afdc5adb9bb3ebcf325b
Victor Jauregui <vsanchezjauregui@gmail.com>
2024-05-26 12:21:35 +0100

    added variables to fct_reviews

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -22,5 +22,12 @@ FROM
 WHERE
     review_text IS NOT NULL
 {% if is_incremental() %}
-  and review_date >= (select max(review_date) from {{ this }})
+    {% if var('start_date', False)  and var('end_date',False)%}
+        {{ log('Loading ' ~ this ~ ' incrementally(start_date: ' ~ var('start_date') ~ ', end_date: ' ~ var('end_date') ~ ')', info=True) }}
+        AND review_date >= '{{ var('start_date') }}'
+        AND review_date < '{{ var('end_date') }}'
+    {% else %}
+        AND review_date >= (select max(review_date) from {{ this }})
+        {{ log('Loading ' ~ this ~ ' incrementally(all missing dates)', info=True) }}
+    {% endif %}
 {% endif %}
