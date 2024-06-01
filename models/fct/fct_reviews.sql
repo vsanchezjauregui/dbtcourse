1d02a86cc727eb00882a380204cd6c5c09339e86
Victor Jauregui <victor.jauregui@outsystems.com>
2024-06-01 12:18:07 +0100

    Revert "added variables to fct_reviews"
    This reverts commit 48ede58c4521a05ac3a6afdc5adb9bb3ebcf325b.

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -22,12 +22,5 @@ FROM
 WHERE
     review_text IS NOT NULL
 {% if is_incremental() %}
-    {% if var('start_date', False)  and var('end_date',False)%}
-        {{ log('Loading ' ~ this ~ ' incrementally(start_date: ' ~ var('start_date') ~ ', end_date: ' ~ var('end_date') ~ ')', info=True) }}
-        AND review_date >= '{{ var('start_date') }}'
-        AND review_date < '{{ var('end_date') }}'
-    {% else %}
-        AND review_date >= (select max(review_date) from {{ this }})
-        {{ log('Loading ' ~ this ~ ' incrementally(all missing dates)', info=True) }}
-    {% endif %}
+  and review_date >= (select max(review_date) from {{ this }})
 {% endif %}
